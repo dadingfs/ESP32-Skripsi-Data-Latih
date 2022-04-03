7250bd717cb31747889692c380395c1eaf0a0004
Dading F Sukma <dadingfs@gmail.com>
2022-04-03 12:51:37 +0700

    Versi 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,173 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
 
-void setup() {
-  // put your setup code here, to run once:
+int button1 = 34;
+int button2 = 27;
+int button3 = 13;
+int button4 = 35;
+int button5 = 25;
+int button6 = 14;
+int button7 = 32;
+int button8 = 26;
+int button9 = 12;
+
+// Wifi
+WiFiMulti wifiMulti;
+
+String listSSID[6] = {"AP1", "AP2", "AP3", "AP4", "AP5", "AP6"};
+int RSSIWifi[6] = {0, 0, 0, 0, 0, 0};
+
+void scanWifi()
+{
+  int n = WiFi.scanNetworks();
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < 6; j++)
+      if (WiFi.SSID(i) == listSSID[j])
+      {
+        RSSIWifi[j] = WiFi.RSSI(i);
+      }
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Accessed Google Script Settings
+const char *APP_SERVER = "script.google.com";
+const char *KEY = "AKfycbymONBRL88A0yPpcBoD-JovH_fiTTkxRy1X-dqN4er3Da9-SFqogIViPOTAyp9PBar80Q";
+
+// SpreadSheet
+void uploadData(int ap1, int ap2, int ap3, int ap4, int ap5, int ap6, int ruangan)
+{
+  HTTPClient http;
+  String URL = "https://script.google.com/macros/s/";
+
+  URL += KEY;
+  URL += "/exec?";
+  URL += "ap1=";
+  URL += ap1;
+  URL += "&ap2=";
+  URL += ap2;
+  URL += "&ap3=";
+  URL += ap3;
+  URL += "&ap4=";
+  URL += ap4;
+  URL += "&ap5=";
+  URL += ap5;
+  URL += "&ap6=";
+  URL += ap6;
+  URL += "&ruangan=";
+  URL += ruangan;
+
+  Serial.println("[HTTP] begin...");
+  Serial.println(URL);
+  // access to your Google Sheets
+  Serial.println();
+  // configure target server and url
+  http.begin(URL);
+  http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+
+  Serial.println("[HTTP] GET...");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if (httpCode > 0)
+  {
+    // HTTP header has been send and Server response header has been handled
+    Serial.print("[HTTP] GET... code: ");
+    Serial.println(httpCode);
+
+    // file found at server
+    String payload = http.getString();
+    Serial.println(payload);
+  }
+  else
+  {
+    Serial.print("[HTTP] GET... failed, error: ");
+    Serial.println(http.errorToString(httpCode).c_str());
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  pinMode(button4, INPUT);
+  pinMode(button5, INPUT);
+  pinMode(button6, INPUT);
+  pinMode(button7, INPUT);
+  pinMode(button8, INPUT);
+  pinMode(button9, INPUT);
+
+  wifiMulti.addAP("AP1", "123456789");
+  wifiMulti.addAP("AP2", "123456789");
+  wifiMulti.addAP("AP3", "123456789");
+  wifiMulti.addAP("AP4", "123456789");
+  wifiMulti.addAP("AP5", "123456789");
+  wifiMulti.addAP("AP6", "123456789");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(500);
+}
+
+void loop()
+{
+  int ruangan = 0;
+
+  if (digitalRead(button1) == HIGH)
+  {
+    ruangan = 1;
+  }
+  if (digitalRead(button2) == HIGH)
+  {
+    ruangan = 2;
+  }
+  if (digitalRead(button3) == HIGH)
+  {
+    ruangan = 3;
+  }
+  if (digitalRead(button4) == HIGH)
+  {
+    ruangan = 4;
+  }
+  if (digitalRead(button5) == HIGH)
+  {
+    ruangan = 5;
+  }
+  if (digitalRead(button6) == HIGH)
+  {
+    ruangan = 6;
+  }
+  if (digitalRead(button7) == HIGH)
+  {
+    ruangan = 7;
+  }
+  if (digitalRead(button8) == HIGH)
+  {
+    ruangan = 8;
+  }
+  if (digitalRead(button9) == HIGH)
+  {
+    ruangan = 9;
+  }
+
+  if (ruangan == 0)
+  {
+    delay(1000);
+  }
+  else
+  {
+    scanWifi();
+    if (wifiMulti.run() == WL_CONNECTED)
+    {
+      Serial.println(WiFi.SSID());
+      uploadData(RSSIWifi[0], RSSIWifi[1], RSSIWifi[2], RSSIWifi[3], RSSIWifi[4], RSSIWifi[5], ruangan);
+      WiFi.disconnect();
+    }
+  }
 }
